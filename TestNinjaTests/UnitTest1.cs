c6f1db505d890915771631df0289d981eff5310e
Spocin <7z\kt\h.TS:87izb5\*dM'|7h>
2022-05-08 13:10:21 +0200

    Created project to test Booking Helper.

--- TestNinjaTests/UnitTest1.cs
+++ TestNinjaTests/UnitTest1.cs
@@ -2,17 +2,8 @@ using NUnit.Framework;
 
 namespace TestNinjaTests
 {
-    public class Tests
+    public class BookingHelper_OverlappingBookingExistsTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+        
     }
 }
